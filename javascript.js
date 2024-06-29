9dd549c5ec524c1e52643426707ce46c930c61b1
SHRIYA CHATURVEDI <98899726+Shriyachaturvedi123@users.noreply.github.com>
2024-06-29 16:28:55 +0530

    Portfolio

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function sign(){
+  alert("Congrats")
+}
+
+// function next(){
+//   alert("Sign in")
+// }
+
+function moveToNextPage() {
+  // Navigate to the next page
+  window.location.href = "login.html";
+}
+
+function forgot(){
+  alert("password change")
+  prompt("put your password")
+  confirm("yes or no")
+}
+
+function submit(){
+  window.location.href="index.html";
+}
